fe63301e5961d0006f25601bf5d8a0677fb1ceb5
henrique <iklobato1@gmail.com>
2024-04-01 22:03:34 -0300

    django init

--- desvio/settings.py
+++ desvio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4g#qw&85wbv8vz%asxsgvonaep3lug08xgs2cjpakm-d@0q1po"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
